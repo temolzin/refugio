64eb9e1e7a8cfeb3add48387a9046592d2bbe59c
af205404 <86862662+af205404@users.noreply.github.com>
2024-05-21 15:29:08 -0600

    Update adminlte.php
    Remove loading animation

--- config/adminlte.php
+++ config/adminlte.php
@@ -112,7 +112,7 @@ return [
     */
 
     'preloader' => [
-        'enabled' => true,
+        'enabled' => false,
         'mode' => 'fullscreen',
         'img' => [
             'path' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
