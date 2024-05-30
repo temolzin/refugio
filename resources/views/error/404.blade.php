ae401d7c889dfff7c9f913ad4c53fcbd95141a2c
Carmen <carmenlopezsanjuan@gmail.com>
2024-05-29 18:05:13 -0600

    style

--- resources/views/error/404.blade.php
+++ resources/views/error/404.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>404 - Página no encontrada</title>
-    <link rel="stylesheet" href="{{ asset('css/errorPage/errorPage.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/errorPage/error404.css') }}">
 </head>
 <body>
     <img src="/img/Logo.png" class="error-image">
