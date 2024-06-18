bc3aab506e04de3840a7757194c3054b51385dc4
Carmen Lopez <133041024+Carmenlopez1720@users.noreply.github.com>
2024-06-18 11:39:01 -0600

    Update RoleSeeder.php
    Changing role names

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,21 +15,21 @@ class RoleSeeder extends Seeder
      */
     public function run()
     {
-        $role1 = Role::create(['name'=>'admin']);
-        $role2 = Role::create(['name'=>'shelter']);
+        $roleAdmin = Role::create(['name'=>'admin']);
+        $roleShelter = Role::create(['name'=>'shelter']);
 
-        Permission::create(['name' => 'viewDashboard', 'description' => 'ver dashboard'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'viewUser', 'description' => 'ver usuario'])->assignRole($role1);
-        Permission::create(['name' => 'viewShelter', 'description' => 'ver albergue'])->assignRole($role1);
-        Permission::create(['name' => 'viewRol', 'description' => 'ver rol'])->assignRole($role1);
+        Permission::create(['name' => 'viewDashboard', 'description' => 'ver dashboard'])->syncRoles([$roleAdmin, $roleShelter]);
+        Permission::create(['name' => 'viewUser', 'description' => 'ver usuario'])->assignRole($roleAdmin);
+        Permission::create(['name' => 'viewShelter', 'description' => 'ver albergue'])->assignRole($roleAdmin);
+        Permission::create(['name' => 'viewRol', 'description' => 'ver rol'])->assignRole($roleAdmin);
 
-        Permission::create(['name' => 'viewVaccine', 'description' => 'ver vacuna'])->assignRole($role2);
-        Permission::create(['name' => 'viewSpecie', 'description' => 'ver especie'])->assignRole($role2);
-        Permission::create(['name' => 'viewAnimal', 'description' => 'ver animal'])->assignRole($role2);
-        Permission::create(['name' => 'viewQuotes', 'description' => 'ver citas'])->assignRole($role2);
-        Permission::create(['name' => 'viewAdoptions', 'description' => 'ver adopciones'])->assignRole($role2);
-        Permission::create(['name' => 'viewShelterUsers', 'description' => 'ver usuarios albergue'])->assignRole($role2);
-        Permission::create(['name' => 'viewGodparents', 'description' => 'ver apadrinamientos'])->assignRole($role2);
+        Permission::create(['name' => 'viewVaccine', 'description' => 'ver vacuna'])->assignRole($roleShelter);
+        Permission::create(['name' => 'viewSpecie', 'description' => 'ver especie'])->assignRole($roleShelter);
+        Permission::create(['name' => 'viewAnimal', 'description' => 'ver animal'])->assignRole($roleShelter);
+        Permission::create(['name' => 'viewQuotes', 'description' => 'ver citas'])->assignRole($roleShelter);
+        Permission::create(['name' => 'viewAdoptions', 'description' => 'ver adopciones'])->assignRole($roleShelter);
+        Permission::create(['name' => 'viewShelterUsers', 'description' => 'ver usuarios albergue'])->assignRole($roleShelter);
+        Permission::create(['name' => 'viewGodparents', 'description' => 'ver apadrinamientos'])->assignRole($roleShelter);
 
     }
 }
