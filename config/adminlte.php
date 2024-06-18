57e68b4509955e4dd4d6936eedce9894d9e81200
MiguelSoon <mdelgadosoon2@gmail.com>
2024-06-17 19:05:13 -0600

    select2config

--- config/adminlte.php
+++ config/adminlte.php
@@ -347,6 +347,11 @@ return [
             'icon' => 'fa fa-home',           
         ],
         [
+            'text' => 'Citas Veterinarias',
+            'url' => '/vets',
+            'icon' => 'fa fa-calendar',           
+        ],
+        [
             'text' => 'Roles',
             'url' => '/roles',
             'icon' => 'fas fa-users-cog fa-fw',
@@ -520,16 +525,16 @@ return [
             ],
         ],
         'Select2' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
-                    'asset' => false,
+                    'asset' => true,
                     'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/js/select2.min.js',
                 ],
                 [
                     'type' => 'css',
-                    'asset' => false,
+                    'asset' => true,
                     'location' => '//cdnjs.cloudflare.com/ajax/libs/select2/4.0.3/css/select2.css',
                 ],
             ],
